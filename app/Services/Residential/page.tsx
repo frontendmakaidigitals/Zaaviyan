eb678c2e4a9a882dc592a6a270403cae5616cd76
faheem221 <faheemk793@gmail.com>
2025-03-19 11:50:23 +0530

    add bg layer

--- app/Services/Residential/page.tsx
+++ app/Services/Residential/page.tsx
@@ -11,8 +11,8 @@ const Page = () => {
 
   return (
     <main className="">
-      <div className="relative">
-        <div className="w-[500px] absolute right-0 -z-10 bottom-10  h-[400px] bg-slate-100">
+      <div className="relative ">
+        <div className="w-[500px] absolute right-0 -z-10 bottom-0  h-[400px] bg-slate-100">
           <img
             src={
               "https://images.unsplash.com/photo-1631679706909-1844bbd07221?q=80&w=3192&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -20,7 +20,8 @@ const Page = () => {
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="grid h-[750px] relative container place-items-center grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="h-full w-full max-w-3xl -z-20 bg-gradient-to-b from-orange-100 absolute right-0 tpo-0 " />
+        <div className="grid h-[730px] relative container place-items-center grid-cols-1 lg:grid-cols-2 gap-4">
           <div className="h-full left-0 absolute max-w-xl flex flex-col items-start justify-center">
             <h2 className="text-6xl bg-[#F7F7F7] p-2 font-heroFont">
               Interior{" "}
@@ -57,7 +58,7 @@ const Page = () => {
             </div>
           </div>
 
-          <div className="w-full h-full flex items-center absolute max-w-[750px] right-10 -z-10">
+          <div className="w-full h-full flex items-center absolute max-w-3xl right-8 -z-10">
             <motion.img
               src={
                 "https://images.unsplash.com/photo-1537726235470-8504e3beef77?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
